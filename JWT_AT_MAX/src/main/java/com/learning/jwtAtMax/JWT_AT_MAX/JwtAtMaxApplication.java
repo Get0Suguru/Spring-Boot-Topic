e9b188ff01c43a04cbf064e94fa4ee02e0d6df35
Gautam Taneja <gautam45taneja@gmail.com>
2025-05-08 11:43:22 +0530

    practiced jwt again

---
+++ JWT_AT_MAX/src/main/java/com/learning/jwtAtMax/JWT_AT_MAX/JwtAtMaxApplication.java
@@ -0,0 +1,13 @@
+package com.learning.jwtAtMax.JWT_AT_MAX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAtMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAtMaxApplication.class, args);
+	}
+
+}
---
+++ JWT_AT_MAX/src/test/java/com/learning/jwtAtMax/JWT_AT_MAX/JwtAtMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.jwtAtMax.JWT_AT_MAX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAtMaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
